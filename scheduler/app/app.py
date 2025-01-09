72d14ad7b1fddedafd1ac9462a68d9da11bf8352
BHarris02 <118061899+BHarris02@users.noreply.github.com>
2025-01-09 13:05:07 +0000

    Rabbitmq integration for dispatching tasks

--- scheduler/app/app.py
+++ scheduler/app/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify, json
+import pika
 
 app = Flask(__name__)
+
+rabbitmq_host = "localhost"
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
+channel = connection.channel()
+channel.queue_declare(queue="task_queue")
 
 tasks: dict = {}
 
@@ -8,9 +14,18 @@ tasks: dict = {}
 def create_task() -> json:
     task_id = len(tasks) + 1
     data = request.json
-    tasks[task_id] = {"task_id": task_id,
+    task = {"task_id": task_id,
                       "status": "scheduled",
                       "data": data}
+    tasks[task_id] = task
+    
+    channel.basic_publish(
+        exchange='',
+        routing_key="task_queue",
+        body=str(task)
+    )
+    print(f"[x] Sent {task}")
+
     return jsonify({
         "message": "Task created",
         "task_id": task_id
--- workers/src/main/java/com/taskworker/worker.java
+++ workers/src/main/java/com/taskworker/worker.java
@@ -1,8 +1,29 @@
 package com.taskworker;
 
+import com.rabbitmq.client.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class Worker {
+    private static final String QUEUE_NAME = "task_queue";
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         System.out.println("Worker is running...");
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for Messages - To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String msg = new String(delivery.getBody(), StandardCharsets.UTF-8);
+            System.out.println(" [x] Received '" + message + "'");
+            System.out.println(" [x] Done");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
     }
 }
